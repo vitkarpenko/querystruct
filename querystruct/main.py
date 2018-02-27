4296ab97287c6da47b7484e2982ee6201fd096a3
Vitaly Karpenko <v.karpenko@atol.ru>
2018-02-27 17:32:53 +0300

    OR works

--- querystruct/main.py
+++ querystruct/main.py
@@ -30,11 +30,14 @@ class Querystruct:
     def to_sql(self, query=None, parents=None):
         """ По self.query рекурсивно генерирует WHERE часть соответствующего SQL SELECT запроса.
 
-        parents: список всех ключей выше по "дереву" запроса вплоть до текущего
+        parents: список всех ключей выше по "дереву" запроса вплоть до текущего.
+
+        В силу своей рекурсивности генерирует "лишние" скобки, но это не имеет значения,
+        т.к. результат в целом не предназначен для чтения человеком.
 
         Примеры:
-        {'status':'A'} --> '(status = "A")'
-        {'age':{'$gt':'25','$lte':'50'}} --> ('((age > 25) AND (age <= 50))')
+        {'status':'A'} -> '(status = "A")'
+        {'age':{'$gt':'25','$lte':'50'}} -> '((age > 25) AND (age <= 50))'
         """
         if not query:
             query = self.query
@@ -44,18 +47,18 @@ class Querystruct:
         if parents[-1] in self.comparison_operators:
             return f'{parents[-2]} {self.comparison_operators[parents[-1]]} {query}'
 
+        if not parents[-1].startswith('$'):
+            if isinstance(query, str):
+                return f"{parents[-1]} = '{query}'"
+
+        if parents[-1].startswith('$or'):
+            return ' OR '.join(
+                self.to_sql(querypart)
+                for querypart in query
+            )
+
         if not parents[-1] or (len(query) > 1 and parents[-1] != '$or'):
             return ' AND '.join(
                 '(' + self.to_sql(querypart, parents + [parent]) + ')'
                 for parent, querypart in query.items()
             )
-
-        if parents[-1].startswith('$or'):
-            return ' OR '.join(
-                '(' + self.to_sql(querypart, parents + [parent]) + ')'
-                for parent, querypart in query.items()
-            )
-
-        if not parents[-1].startswith('$'):
-            if isinstance(query, str):
-                return f'{parents[-1]} = "{query}"'
--- tests/test_to_sql.py
+++ tests/test_to_sql.py
@@ -4,9 +4,9 @@ from querystruct import Querystruct
 
 
 @pytest.mark.parametrize("query, expected", [
-    ('{"status":"A"}', '(status = "A")'),
+    ('{"status":"A"}', "(status = 'A')"),
     ('{"age":{"$gt":"25","$lte":"50"}}', '((age > 25) AND (age <= 50))'),
-    ('{"or":[{"beer":"meh"},{"beer":"gut"}]}', '((beer = meh) OR (beer = gut))')
+    ('{"$or":[{"beer":"meh"},{"beer":"gut"}]}', "((beer = 'meh') OR (beer = 'gut'))")
 ])
 def test_to_sql(query, expected):
     assert Querystruct(query).to_sql() == expected
