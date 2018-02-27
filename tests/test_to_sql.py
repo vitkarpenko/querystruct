fd1c7dfda1f3f57c2c69e19cdb3e03d95c9f8d2f
Vitaly Karpenko <v.karpenko@atol.ru>
2018-02-27 19:29:22 +0300

    test parameters formatting

--- tests/test_to_sql.py
+++ tests/test_to_sql.py
@@ -4,14 +4,38 @@ from querystruct import Querystruct
 
 
 @pytest.mark.parametrize("query, expected", [
-    ('{"status": "A"}', "(status = 'A')"),
-    ('{"value": "100500"}', "(value = 100500)"),
-    ('{"rabbit": {"$ne": "mq"}}', "((rabbit != 'mq'))"),
-    ('{"age": {"$gt": "25"}}', '((age > 25))'),
-    ('{"age": {"$gt": "25", "$lte": "50"}}', '((age > 25) AND (age <= 50))'),
-    ('{"$or": [{"beer": "meh"}, {"beer": "gut"}]}', "((beer = 'meh') OR (beer = 'gut'))"),
-    ('{"$or": [{"beer": "meh"}, {"beer_price": {"$lt": "50"}}]}', "((beer = 'meh') OR ((beer_price < 50)))"),
-    ('{"bloodbourne": "meh", "diabloIII": "gut"}', "(bloodbourne = 'meh') AND (diabloIII = 'gut')")
+    (
+        '{"status": "A"}',
+        "(status = 'A')"
+    ),
+    (
+        '{"value": "100500"}',
+        "(value = 100500)"
+    ),
+    (
+        '{"rabbit": {"$ne": "mq"}}',
+        "((rabbit != 'mq'))"
+    ),
+    (
+        '{"age": {"$gt": "25"}}',
+        '((age > 25))'
+    ),
+    (
+        '{"age": {"$gt": "25", "$lte": "50"}}',
+        '((age > 25) AND (age <= 50))'
+    ),
+    (
+        '{"$or": [{"beer": "meh"}, {"beer": "gut"}]}',
+        "((beer = 'meh') OR (beer = 'gut'))"
+    ),
+    (
+        '{"$or": [{"beer": "meh"}, {"beer_price": {"$lt": "50"}}]}',
+        "((beer = 'meh') OR ((beer_price < 50)))"
+    ),
+    (
+        '{"bloodbourne": "meh", "diabloIII": "gut"}',
+        "(bloodbourne = 'meh') AND (diabloIII = 'gut')"
+    )
 ])
 def test_to_sql(query, expected):
     assert Querystruct(query).to_sql() == expected
