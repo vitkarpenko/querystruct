d80ac9b52a586f479882073e4e545bfdcfa6ca19
Vitaly Karpenko <v.karpenko@atol.ru>
2018-02-27 19:11:52 +0300

    +test

--- tests/test_to_sql.py
+++ tests/test_to_sql.py
@@ -10,6 +10,7 @@ from querystruct import Querystruct
     ('{"age": {"$gt": "25"}}', '((age > 25))'),
     ('{"age": {"$gt": "25", "$lte": "50"}}', '((age > 25) AND (age <= 50))'),
     ('{"$or": [{"beer": "meh"}, {"beer": "gut"}]}', "((beer = 'meh') OR (beer = 'gut'))"),
+    ('{"$or": [{"beer": "meh"}, {"beer_price": {"$lt": "50"}}]}', "((beer = 'meh') OR ((beer_price < 50)))"),
     ('{"bloodbourne": "meh", "diabloIII": "gut"}', "(bloodbourne = 'meh') AND (diabloIII = 'gut')")
 ])
 def test_to_sql(query, expected):
