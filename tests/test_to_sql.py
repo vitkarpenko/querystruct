15b6ff8df517cbd519a37dab14e3aa2769ca6ac6
Vitaly Karpenko <v.karpenko@atol.ru>
2018-02-27 18:25:45 +0300

    +tests

--- tests/test_to_sql.py
+++ tests/test_to_sql.py
@@ -4,12 +4,13 @@ from querystruct import Querystruct
 
 
 @pytest.mark.parametrize("query, expected", [
-    ('{"status":"A"}', "(status = 'A')"),
-    ('{"value":"100500"}', "(value = 100500)"),
-    ('{"rabbit":{"$ne":"mq"}}', "((rabbit != 'mq'))"),
-    ('{"age":{"$gt":"25"}}', '((age > 25))'),
-    ('{"age":{"$gt":"25","$lte":"50"}}', '((age > 25) AND (age <= 50))'),
-    ('{"$or":[{"beer":"meh"},{"beer":"gut"}]}', "((beer = 'meh') OR (beer = 'gut'))")
+    ('{"status": "A"}', "(status = 'A')"),
+    ('{"value": "100500"}', "(value = 100500)"),
+    ('{"rabbit": {"$ne": "mq"}}', "((rabbit != 'mq'))"),
+    ('{"age": {"$gt": "25"}}', '((age > 25))'),
+    ('{"age": {"$gt": "25", "$lte": "50"}}', '((age > 25) AND (age <= 50))'),
+    ('{"$or": [{"beer": "meh"}, {"beer": "gut"}]}', "((beer = 'meh') OR (beer = 'gut'))"),
+    ('{"bloodbourne": "meh", "diabloIII": "gut"}', "(bloodbourne = 'meh') AND (diabloIII = 'gut')")
 ])
 def test_to_sql(query, expected):
     assert Querystruct(query).to_sql() == expected
