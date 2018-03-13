7a648e9fa836419dab81647efde10193f8ad5eb6
Vitaly Karpenko <v.karpenko@atol.ru>
2018-03-13 14:19:30 +0300

    pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'querystruct',
+    packages = ['querystruct'],
+    version = '0.1',
+    description = 'Translator from pymongo query "language" to pure SQL',
+    author = 'Vitaly Karpenko',
+    author_email = 'vitkarpenko@gmail.com',
+    url = 'https://github.com/vitkarpenko/querystruct',
+    download_url = 'https://github.com/vitkarpenko/querystruct/archive/0.2.tar.gz',
+    keywords = ['pymongo', 'sql', 'translator'],
+    classifiers = [],
+)
